28308b19fee58042b42aef64f8435bb0578601fd
Marc Arza Garcia <129588616+CodeMarc42@users.noreply.github.com>
2026-01-18 13:47:31 +0100

    add one more

--- ft_linux_extra_packages.sh
+++ ft_linux_extra_packages.sh
@@ -55,6 +55,7 @@ check "Make-ca"      bin  make-ca
 check "Nettle"       bin  nettle-hash
 check "p11-kit"      bin  p11-kit
 check "Libxcrypt"    pkg  libxcrypt
+check "Linux-PAM"    pkg  pwhistory_helper
 
 printf "\nText Editors:"
 check "Nano"         bin  nano
